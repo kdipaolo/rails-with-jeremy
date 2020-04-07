d90a829a01c85591bdeb478fedcc5b16fb4dad90
Kurt DiPaolo <kurt.dipaolo@kproductivity.mail.onmicrosoft.com>
2020-04-07 08:49:22 -0400

    remove unused files

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   resources :posts do 
     resources :comments
   end
-  resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "pages#index"
+  
+  # root to: "pages#index"
 
   match '/add-comment' => 'posts#addComment', via: :post
 
-  # match '/star-wars' => 'pages#get_star_wars', via: :get
-  
   controller :users do 
     get 'signup' => :new
     post 'signup' => :create
