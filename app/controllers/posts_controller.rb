c4cdff730974159ad7b39478e82801f81ccfaad0
Kurt DiPaolo <kurt.dipaolo@kproductivity.mail.onmicrosoft.com>
2020-04-15 08:42:41 -0400

    able to add author to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,8 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    
+    @post = Post.new(post_params.merge({user: current_user}))
     
     respond_to do |format|
     if @post.save
