ddc562130e19ae5b8ed502915d1dab36a08fff09
Kurt DiPaolo <kurt.dipaolo@kproductivity.mail.onmicrosoft.com>
2020-05-08 14:10:40 -0400

    comment on post email

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,12 @@ class CommentsController < ApplicationController
     def create 
         @post = Post.find(params[:post_id])
         @comment = @post.comments.build(comment_params.merge({user: current_user}))
-        
-        if @comment.save 
-            redirect_to @post
+        if @comment.save
+            if @post.user 
+                UserMailer.with(post: @post, comment: @comment, currentUser: current_user).comment_on_post_email.deliver_later 
+            end
+                redirect_to @post
+            
         else
             @comments = @post.comments
             render '/posts/show'
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,15 @@ class UserMailer < ApplicationMailer
 
     def welcome_email 
         @user = params[:user]
+        
         mail(to: @user.email, subject: "Welcome!")
+    end
+
+    def comment_on_post_email 
+        @post = params[:post]
+        @comment = params[:comment]
+        @currentUser = params[:currentUser]
+
+        mail(to: @post.user.email, subject: "#{@currentUser.name} has commented on one of your posts!")
     end
 end
